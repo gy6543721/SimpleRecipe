bd934003130d5f7a9846724b5dbe180b705ac580
gy6543721 <gy6543721@yahoo.com.tw>
2022-12-09 17:34:53 +0900

    Fragment Update

--- app/src/main/java/com/example/simplerecipe/MainActivity.kt
+++ app/src/main/java/com/example/simplerecipe/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.simplerecipe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.simplerecipe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navigationController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        navigationController = findNavController(R.id.fragmentContainer)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoritesFragment,
+                R.id.othersFragment
+            )
+        )
+        binding.bottomNavigationView.setupWithNavController(navigationController)
+        setupActionBarWithNavController(navigationController,appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navigationController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/example/simplerecipe/RecipesFragment.kt
+++ app/src/main/java/com/example/simplerecipe/RecipesFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class FavoriteFragment : Fragment() {
+class RecipeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        return inflater.inflate(R.layout.fragment_recipes, container, false)
     }
 
 }
